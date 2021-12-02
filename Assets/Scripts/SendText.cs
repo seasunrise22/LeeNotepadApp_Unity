34758abe895c72d1dac739a6cc6dafed8597d566
seasunrise22 <seasunrise22@gmail.com>
2021-12-02 20:11:15 +0900

    MySQL에 테스트 텍스트 삽입 성공

---
+++ Assets/Scripts/SendText.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SendText : MonoBehaviour
+{
+    public InputField textField;
+    public Button sendButton;
+
+    public void CallSendToDB()
+    {
+        StartCoroutine(SendToDB());
+    }
+
+    IEnumerator SendToDB()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("text", textField.text);
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost/LeeNotepadApp/SendText.php", form);
+        yield return www.SendWebRequest();
+        if(www.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log("ConnectionError");
+        }
+        else
+        {
+            Debug.Log("Send Text to Database successful");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+
+    public void VerifyInputs()
+    {
+        sendButton.interactable = (textField.text.Length >= 1);
+    }
+}
